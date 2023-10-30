3935fc59a986265b1309011eed0e30ee1915eade
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-10-30 14:47:58 +0300

    1 step

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,9 @@
+import StringSchema from './StringSchema.js';
 
+export default class Validator {
+  string() {
+    const validator = (value) => typeof value === 'string';
+    return new StringSchema([validator]);
+  }
+
+}
