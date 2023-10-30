3530cb10989e471b91f15e1291faac1cb1886e89
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-10-30 15:02:04 +0300

    2 step

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -2,9 +2,39 @@ export default class StringSchema {
   constructor(validators) {
     this.validators = [...validators];
   }
-  
+
   isValid(value) {
     return this.validators.every((validator) => validator(value) === true);
   }
   
+  startsFromUpperCase() {
+    const validator = (value) => {
+      const regexp = /^[A-Z]/;
+      if (value.match(regexp) !== null) {
+        return true;
+      }
+      return false;
+    };
+    return new StringSchema([...this.validators, validator]);
+  }
+
+  length(len) {
+    const validator = (value) => {
+      if(value.length === len){
+        return true;
+      }
+      return false;
+    }
+    return new StringSchema([...this.validators, validator]);
+  }
+
+  hasExclamation() {
+    const validator = (value) => {
+      if(value.indexOf("!")>=0){
+        return true;
+      }
+      return false;    
+    }
+    return new StringSchema([...this.validators, validator]);
+  }
 }
