05e0d04e9a4b396938128b4064788dd36be6080c
Anatoliy Tupitsin <hexlet@wailbox.ru>
2023-10-30 15:14:04 +0300

    4 step

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -6,5 +6,26 @@ export default class ArraySchema {
   isValid(value) {
     return this.validators.every((validator) => validator(value) === true);
   }
-  
+
+  maxDepth(dep) {
+    const findDepth = (value, currentDepth) => {
+      let filteredValues = value.filter((arr) => Array.isArray(arr));
+      filteredValues=filteredValues.flat();
+      
+      if(filteredValues.length!== 0) {
+        return findDepth(filteredValues, currentDepth + 1);
+      } else {
+        return currentDepth;
+      }
+    };
+
+    const validator = (value) => {
+      if(findDepth(value, 0) <= dep){
+        return true;
+      }
+      return false;
+    }
+
+    return new ArraySchema([...this.validators, validator]);
+  }  
 }
